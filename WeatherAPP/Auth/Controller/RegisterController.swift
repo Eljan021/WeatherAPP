4d0ca05e549cccb376bbd961caf546271ddae23a
Eljan021 <elcan021@bk.ru>
2024-04-24 20:01:44 +0400

    Created RegisterController

---
+++ WeatherAPP/Auth/Controller/RegisterController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterController.swift
+//  WeatherAPP
+//
+//  Created by Eljan on 24.04.24.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
