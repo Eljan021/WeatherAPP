81c67fc8efbb85cf3ba9efe7d9810187a6cbd1b5
Eljan021 <elcan021@bk.ru>
2024-04-28 13:58:18 +0400

    textFieldDidChangeSelection add

--- WeatherAPP/Auth/Controller/RegisterViewController.swift
+++ WeatherAPP/Auth/Controller/RegisterViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet private weak var userNameLabel: UILabel!
     @IBOutlet private weak var userNameTextField: UITextField!
     @IBOutlet private weak var passwordLabel: UILabel!
@@ -28,11 +28,51 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationController?.setNavigationBarHidden(true, animated: false)
+        userNameTextField.delegate = self
+        passwordTextField.delegate = self
+        emailTextField.delegate = self
+        surnameNameTextField.delegate = self
         
-//        func backtologim(){
-//            navigationController?.popViewController(animated: true)
-//        }
+        
+    }
+    
+}
 
-        // Do any additional setup after loading the view.
+
+extension RegisterViewController: UITextFieldDelegate {
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        switch textField {
+        case emailTextField:
+            if (emailTextField.text?.count ?? 0) > 3 {
+                emailTextField.layer.borderWidth = 0
+            }else{
+                emailTextField.layer.borderWidth = 1
+                emailTextField.layer.borderColor = UIColor.red.cgColor
+            }
+        case passwordTextField:
+            if (passwordTextField.text?.count ?? 0) > 3 {passwordTextField.layer.borderWidth = 0 } else {
+                passwordTextField.isSecureTextEntry = true
+                passwordTextField.layer.borderWidth = 1
+                passwordTextField.layer.borderColor = UIColor.red.cgColor
+            }
+        case userNameTextField:
+            if (userNameTextField.text?.count ?? 0) > 3 {userNameTextField.layer.borderWidth = 0} else {
+                userNameTextField.layer.borderWidth = 1
+                userNameLabel.layer.borderColor = UIColor.red.cgColor
+                
+            }
+        case surnameNameTextField:
+            if (surnameNameTextField.text?.count ?? 0) > 3 {surnameNameTextField.layer.borderWidth = 0} else {
+                surnameNameTextField.layer.borderWidth = 1
+                surnameNameTextField.layer.borderColor = UIColor.red.cgColor
+            }
+            
+            
+        default:
+            break
+        }
     }
 }
+
+
