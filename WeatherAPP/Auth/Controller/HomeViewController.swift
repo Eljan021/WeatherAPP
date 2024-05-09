a954688b300a6c53434e62bcf54a5915a02481c7
Eljan021 <elcan021@bk.ru>
2024-05-09 19:28:47 +0400

    home add collection view cell

---
+++ WeatherAPP/Auth/Controller/HomeViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HomeViewController.swift
+//  WeatherAPP
+//
+//  Created by Eljan on 08.05.24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var collection: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collection.registerNib(with: "HomeCollectionViewCell")
+        
+    }
+    
+
+
+}
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionViewCell", for: indexPath) as? HomeCollectionViewCell ?? HomeCollectionViewCell()
+        return cell
+    }
+    
+
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
+        
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: 500, height: 700)
+    }
+    
+}
---
+++ WeatherAPP/Auth/Views/Cells/HomeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  HomeCollectionViewCell.swift
+//  WeatherAPP
+//
+//  Created by Eljan on 08.05.24.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
