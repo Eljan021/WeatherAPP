47674b74cef005d8c6d55191ac9b6d3eeeefd9eb
Eljan021 <elcan021@bk.ru>
2024-05-14 14:29:43 +0400

    Created ViewModel

---
+++ WeatherAPP/Auth/Model/WeatherModel.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherModel.swift
+//  WeatherAPP
+//
+//  Created by Eljan on 14.05.24.
+//
+
+import Foundation
+
+struct WeatherModel {
+    var temperature: String?
+    var weather: String?
+    var windSpeed: String?
+    var calendarDate: String?
+    var weekDay: String?
+}
---
+++ WeatherAPP/Auth/ViewModel/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  WeatherAPP
+//
+//  Created by Eljan on 14.05.24.
+//
+
+import Foundation
+
+protocol WeatherProtocol: AnyObject {
+    
+}
